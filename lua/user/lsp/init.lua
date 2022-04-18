e6b87126eae15ddd853c578720899605f83c970e
Anes <marzoukianas@gmail.com>
2022-04-18 03:13:39 +0100

    Add null-ls

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -6,5 +6,6 @@ end
 
 require("user.lsp.lsp-installer")
 require("user.lsp.handlers").setup()
+require("user.lsp.null-ls")
 
 
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -119,6 +119,11 @@ _G.packer_plugins = {
     path = "/home/anes/.local/share/nvim/site/pack/packer/start/monokai-pro.nvim",
     url = "https://gitlab.com/__tpb/monokai-pro.nvim"
   },
+  ["null-ls.nvim"] = {
+    loaded = true,
+    path = "/home/anes/.local/share/nvim/site/pack/packer/start/null-ls.nvim",
+    url = "https://github.com/jose-elias-alvarez/null-ls.nvim"
+  },
   ["nvim-autopairs"] = {
     loaded = true,
     path = "/home/anes/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
