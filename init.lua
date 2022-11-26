e95ae8d99ce49c75162960dfb5ee1878d8c218d2
Anes <marzoukianas@gmail.com>
2022-11-26 13:29:39 +0100

    init

--- init.lua
+++ init.lua
@@ -1,18 +1,1 @@
--- TODO:
-  -- add karb94/neoscroll.nvim,
-  -- DONE: change surround to vimsurround, or configure accordinly
-  -- install toggleterm
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.colorscheme"
-require "user.cmp"
-require "user.lsp"
-require "user.telescope"
-require "user.treesitter"
-require "user.autopairs"
-require "user.comment"
-require "user.nvim-tree"
-require "user.lualine"
-require "user.colorizer"
-require "user.toggleterm"
+require "base"
--- lua/user/colorschemes/solarized.lua
+++
@@ -1,5 +0,0 @@
-vim.g.solarized_italics = 0
-vim.g.solarized_visibility = 'high' -- low, normal, high
-vim.g.solarzied_diffmode = 'hight'   -- low, normal, high
-vim.g.solarized_termtrans = 0
-vim.g.solarized_statusline = 'normal'
