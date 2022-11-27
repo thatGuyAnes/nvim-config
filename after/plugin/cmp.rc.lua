9c9cd99e8988168be78cbe52023a2dd93b7e1fb3
Anes <marzoukianas@gmail.com>
2022-11-27 11:57:15 +0100

    fix completion menu

--- after/plugin/cmp.rc.lua
+++ after/plugin/cmp.rc.lua
@@ -1,6 +1,21 @@
+-- default config at :
+-- https://github.com/hrsh7th/nvim-cmp/blob/main/lua/cmp/config/default.lua#L92
+
 local status, cmp = pcall(require, "cmp")
 if (not status) then return end
 local lspkind = require 'lspkind'
+
+local border = {
+  { "╭", "CmpBorder" },
+  { "─", "CmpBorder" },
+  { "╮", "CmpBorder" },
+  { "│", "CmpBorder" },
+  { "╯", "CmpBorder" },
+  { "─", "CmpBorder" },
+  { "╰", "CmpBorder" },
+  { "│", "CmpBorder" },
+}
+local WIDE_HEIGHT = 40
 
 cmp.setup({
   snippet = {
@@ -8,11 +23,28 @@ cmp.setup({
       require('luasnip').lsp_expand(args.body) -- For use of 'luasnip'
     end,
   },
+  window = {
+    completion = {
+      border = border,
+      winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None',
+      scrolloff = 0,
+      col_offset = 0,
+      side_padding = 1,
+      scrollbar = true,
+    },
+    documentation = {
+      max_height = math.floor(WIDE_HEIGHT * (WIDE_HEIGHT / vim.o.lines)),
+      max_width = math.floor((WIDE_HEIGHT * 2) * (vim.o.columns / (WIDE_HEIGHT * 2 * 16 / 9))),
+      border = border,
+      winhighlight = 'FloatBorder:NormalFloat',
+    },
+  },
   mapping = cmp.mapping.preset.insert({
     ['<C-b>'] = cmp.mapping.scroll_docs(-2),
     ['<C-f>'] = cmp.mapping.scroll_docs(2),
     ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-    ['<C-e>'] = cmp.mapping.close(),
+    --['<C-e>'] = cmp.mapping.close(),
+    ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true
@@ -25,6 +57,10 @@ cmp.setup({
   }),
   formatting = {
     format = lspkind.cmp_format({ with_text = false, maxwidth = 50 })
+  },
+  confirm_opts = {
+    behavior = cmp.ConfirmBehavior.Replace,
+    select = false,
   }
 })
 
