e4ac7a571af88ec9b4a5cdff9b3d4c5b6571d7b2
Anes <marzoukianas@gmail.com>
2022-11-27 12:29:20 +0100

    add fold

--- lua/user/base.lua
+++ lua/user/base.lua
@@ -1,52 +1,57 @@
 local options = {
-  termguicolors   = true,
-  autoindent      = true,
-  backup          = false,
-  clipboard       = "unnamedplus",
-  cmdheight       = 2,
-  completeopt     = { "menuone", "noselect" },
-  conceallevel    = 0,
-  cursorline      = true,
-  expandtab       = true,
-  equalalways     = true,
-  fileencoding    = "utf-8",
-  guifont         = "monospace:h17",
-  hlsearch        = false,
-  hidden          = true,
-  ignorecase      = true,
-  incsearch       = true,
-  laststatus	    = 2,
-  mouse           = "a",
-  number          = true,
-  numberwidth     = 4,
-  pumheight       = 10,
-  showmode        = true,
-  showtabline     = 2,
-  shortmess       = "c",
-  sidescrolloff   = 20,
-  signcolumn      = "yes",
-  relativenumber  = true,
-  smartcase       = true,
-  smartindent     = true,
-  smarttab        = true,
-  splitbelow      = true,
-  scrolloff       = 10,
-  softtabstop     = 2,
-  shiftwidth      = 2,
-  splitright      = true,
-  swapfile        = false,
-  tabstop         = 2,
-  timeoutlen      = 500,
-  undofile        = true,
-  updatetime      = 300,
-  wrap            = false,
-  writebackup     = false,
-  foldmarker      = "{,}",
+  termguicolors  = true,
+  autoindent     = true,
+  backup         = false,
+  clipboard      = "unnamedplus",
+  cmdheight      = 2,
+  completeopt    = { "menuone", "noselect" },
+  conceallevel   = 0,
+  cursorline     = true,
+  expandtab      = true,
+  equalalways    = true,
+  fileencoding   = "utf-8",
+  guifont        = "monospace:h17",
+  hlsearch       = false,
+  hidden         = true,
+  ignorecase     = true,
+  incsearch      = true,
+  laststatus     = 2,
+  mouse          = "a",
+  number         = true,
+  numberwidth    = 4,
+  pumheight      = 10,
+  showmode       = true,
+  showtabline    = 2,
+  shortmess      = "c",
+  sidescrolloff  = 20,
+  signcolumn     = "yes",
+  relativenumber = true,
+  smartcase      = true,
+  smartindent    = true,
+  smarttab       = true,
+  splitbelow     = true,
+  scrolloff      = 10,
+  softtabstop    = 2,
+  shiftwidth     = 2,
+  splitright     = true,
+  swapfile       = false,
+  tabstop        = 2,
+  timeoutlen     = 500,
+  undofile       = true,
+  updatetime     = 300,
+  wrap           = false,
+  writebackup    = false,
+  foldmarker     = "{,}",
 }
 
 vim.opt.shortmess:append "c" -- Dont's pass messages to |ins-completion-menu
-vim.wo.wrap = true
-vim.wo.colorcolumn     = '81'
+vim.wo.wrap        = true
+vim.wo.colorcolumn = '81'
+
+-- fold
+vim.wo.foldmethod = 'expr'
+vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.opt.guicursor = ""
 
 for k, v in pairs(options) do
   vim.opt[k] = v
@@ -63,8 +68,8 @@ au! BufEnter * set fo-=co fo-=r fo-=o
 ]])
 
 -- nohilights, highlights all matches while searching
-	-- You can use the CTRL-G and CTRL-T keys to move to the next and
-	-- previous match. |c_CTRL-G| |c_CTRL-T|
+-- You can use the CTRL-G and CTRL-T keys to move to the next and
+-- previous match. |c_CTRL-G| |c_CTRL-T|
 vim.api.nvim_command([[
 		augroup vimrc-incsearch-highlight
 		  autocmd CmdlineEnter /,\? :set hlsearch
